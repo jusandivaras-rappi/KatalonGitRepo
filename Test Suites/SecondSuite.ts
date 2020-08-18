ff02832faafd1232f9db44a67657b45bd93bd1d7
juan.usandivaras <juan.usandivaras@AR-IT13429.globant.com>
2020-08-18 12:20:30 -0300

    Second suite added

--- Test Suites/SecondSuite.ts
+++ Test Suites/SecondSuite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>SuitePrueba</name>
+   <name>SecondSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,7 +10,7 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>029a1faa-29be-42c6-b070-5a487a1fa26f</testSuiteGuid>
+   <testSuiteGuid>bd74e333-e3b6-48f5-9ef1-eab6fb8e6fe3</testSuiteGuid>
    <testCaseLink>
       <guid>5a8096da-2e85-40e0-9b3f-7062fc562fb7</guid>
       <isReuseDriver>false</isReuseDriver>
